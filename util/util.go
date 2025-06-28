6014485322f6e68c70a77f66ed3bd11166a14648
Lars Janssen <lars.e.janssen@protonmail.com>
2025-06-28 15:08:04 +0200

    removed unused method

--- util/util.go
+++ util/util.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,16 +10,6 @@ func JSONResponse(w http.ResponseWriter, data interface{}, status int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	_ = json.NewEncoder(w).Encode(data)
-}
-
-func ItoSmap(m map[interface{}]interface{}) map[string]string {
-	stringMap := map[string]string{}
-	for k, v := range m {
-		key := fmt.Sprintf("%s", k)
-		value := fmt.Sprintf("%s", v)
-		stringMap[key] = value
-	}
-	return stringMap
 }
 
 func Check(e error) {
